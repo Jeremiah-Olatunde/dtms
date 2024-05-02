0ee15039dbcb521562bbeba905ecce4c2e05fed3
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-05-02 02:05:25 +0100

    review model | information on reviews

---
+++ src/models/model-review.ts
@@ -0,0 +1,54 @@
+import {
+  Model,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+} from "sequelize";
+import { sequelize } from "./db.js";
+
+class Review extends Model<
+  InferAttributes<Review>,
+  InferCreationAttributes<Review>
+> {
+  declare uid: string;
+  declare text: string;
+  declare rating: number;
+  declare design: string;
+  declare tailor: string;
+  declare client: string;
+}
+
+Review.init(
+  {
+    uid: {
+      unique: true,
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.CHAR(21),
+    },
+    design: {
+      allowNull: false,
+      type: DataTypes.CHAR(21),
+    },
+    tailor: {
+      allowNull: false,
+      type: DataTypes.CHAR(21),
+    },
+    client: {
+      allowNull: false,
+      type: DataTypes.CHAR(21),
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isNumeric: true,
+        min: 0,
+        max: 10,
+      },
+    },
+    text: DataTypes.TEXT,
+  },
+  { sequelize, modelName: "Review" },
+);
+
+export { Review };
