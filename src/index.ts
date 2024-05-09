62be06e02901f7cc6824b35b3e4d1515e87a1f5a
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-05-09 13:16:12 +0100

    contact page | setup and mounted contact router. updated contact links. added compressed contact image

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ import MySQLStore from "express-mysql-session";
 import { router as home } from "./routes/router-home.js";
 import { router as catalog } from "./routes/router-catalog.js";
 import { router as tailors } from "./routes/router-tailors.js";
+import { router as contact } from "./routes/router-contact.js";
 
 const PORT = process.env.PORT || 8080;
 const DIRECTORY = dirname(fileURLToPath(import.meta.url));
@@ -94,3 +95,4 @@ app.get("/", (_, response) => response.redirect("/home"));
 app.use("/home", home);
 app.use("/catalog", catalog);
 app.use("/tailors", tailors);
+app.use("/contact", contact);
---
+++ src/routes/router-contact.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+export const router = Router();
+
+
+router.get("/", (_, response) => {
+  response.render("view-contact.njk");
+})
