5ac0a2d4fbc434b00bc0e211337a38b284e21a10
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-05-11 02:45:15 +0100

    setting up and mouting signup router

--- src/routes/router-auth.ts
+++ src/routes/router-auth.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
+import { MEASUREMENTS } from "../models/model-user-client.js";
+import { SOCIALS } from "../models/model-user-tailor.js";
 
 export const router = Router();
 
 router.get("/login", (_, response) => {
   response.render("view-auth-login.njk");
+});
+
+router.get("/signup", (_, response) => {
+  response.render("view-auth-signup.njk", { MEASUREMENTS, SOCIALS });
+});
+
+router.post("/signup", (request, _) => {
+  console.log(request.body);
 });
